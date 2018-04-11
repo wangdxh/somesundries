1362401ca1592fb97ff169fccea7de181301a049
wangdxh <wangdxh@163.com>
2018-04-11 17:28:54 +0800

    add

--- swift/mkdevice.sh
+++ swift/mkdevice.sh
@@ -3,7 +3,7 @@ do
     echo /opt/d$i  
     mkdir -p /srv/node/d$i
     dd if=/dev/zero of=/opt/d$i bs=1M count=0 seek=5000
-    mkfs.xfs -f /opt/d$i
+    mkfs.xfs -f -L d$i /opt/d$i
     mount -t xfs -o noatime,nodiratime,logbufs=8 /opt/d$i /srv/node/d$i
 done  
 useradd swift
