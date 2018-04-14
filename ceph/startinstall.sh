c1f9296a9fb20b0bc0e77bdb00de6e2ba121899b
wangdxh <wangdxh@163.com>
2018-04-14 12:42:20 +0800

    add new

--- ceph/startinstall.sh
+++ ceph/startinstall.sh
@@ -2,6 +2,10 @@
 
 systemctl restart ceph-mon@node2
 
+systemctl restart ceph-mgr@node2
+
+sleep 5
+
 cd /root
 sh ./cleanosd.sh
 
@@ -12,6 +16,7 @@ do
     losetup /dev/loop$i /srv/sd$i
     ceph-disk prepare --cluster ceph --cluster-uuid 7ea7741f-c0fa-477e-b0a3-11af8080bad2 /dev/loop$i
     ceph-disk activate /dev/loop${i}p1 --activate-key /etc/ceph/ceph.client.bootstrap-osd.keyring 
+    sleep 10
 done
 
 
