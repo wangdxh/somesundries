7430bc3d3f7f4b445e26f0b6f3ebf1be122212bf
wangxiaohui <wangdxh@163.com>
2018-04-17 15:32:09 +0800

    Update mkdevice.sh

--- swift/mkdevice.sh
+++ swift/mkdevice.sh
@@ -4,7 +4,7 @@ useradd swift
 for ((i=1; i<=4; i ++))  
 do  
     echo loop$i  
-    truncate -s 5GB /srv/swift-disk$i
+    truncate -s 4GB /srv/swift-disk$i
     mkfs.xfs -f /srv/swift-disk$i
     mkdir /mnt/sdb$i    
     mount -t xfs -o loop,noatime,nodiratime,logbufs=8 /srv/swift-disk$i /mnt/sdb$i
@@ -16,5 +16,17 @@ do
 done  
 mkdir -p /var/run/swift
 chown -R swift:swift /var/run/swift
+
+
+rm -rf /etc/swift
+mkdir -p /etc/swift
+
+cd $HOME/swift/doc
+cp -r saio/swift /etc/swift
+cd 
+chown -R swift:swift /etc/swift
+
+find /etc/swift/ -name \*.conf | xargs sudo sed -i "s/<your-user-name>/swift/"
+
 
 
